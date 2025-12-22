3e5de31434b28ac2ed3bdb00bbd67f2a17db3adc
Adrian Frith <adrianf@da.org.za>
2025-12-23 00:06:42 +0200

    add background to map loading spinner

--- app/display/spinner-fill.tsx
+++ app/display/spinner-fill.tsx
@@ -6,7 +6,7 @@ export default function SpinnerFill({
   children?: React.ReactNode;
 }) {
   return (
-    <div className="w-100 h-100 d-flex justify-content-center align-items-center">
+    <div className="w-100 h-100 d-flex justify-content-center align-items-center bg-light">
       <Spinner animation="border" role="status">
         <span className="visually-hidden">Loading...</span>
       </Spinner>
