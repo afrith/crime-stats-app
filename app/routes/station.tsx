37badc3e9423e73fbcf2be34690af44439f5b531
Adrian Frith <adrianf@da.org.za>
2025-12-28 03:30:12 +0200

    fetch crime list and station stats

--- app/routes/station.tsx
+++ app/routes/station.tsx
@@ -1,14 +1,21 @@
 import { data } from "react-router";
 import type { Route } from "./+types/station";
 import { getStationDetails } from "~/db/stations";
+import { getCrimes } from "~/db/crimes";
 import StationView from "~/station-view";
+import { getAnnualStats } from "~/db/stats";
 
 export async function loader({ params }: Route.LoaderArgs) {
-  const station = await getStationDetails(params.stationSlug);
+  const [station, crimes, stats] = await Promise.all([
+    getStationDetails(params.stationSlug),
+    getCrimes(),
+    getAnnualStats({ stationSlug: params.stationSlug }),
+  ]);
+
   if (station == null) {
     throw data("Station Not Found", { status: 404 });
   }
-  return { station };
+  return { station, crimes, stats };
 }
 
 export function meta({ loaderData }: Route.MetaArgs) {
