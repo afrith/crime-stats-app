97a943c770d7ef8b0067812eede312ca527465a8
Adrian Frith <adrianf@da.org.za>
2025-12-29 23:28:41 +0200

    Breadcrumbs on station view

--- app/station-view/index.tsx
+++ app/station-view/index.tsx
@@ -1,19 +1,25 @@
 import type { Route } from "../routes/+types/station";
 import StationMap from "~/map/station-map.client";
 import ClientOnly from "~/utils/client-only";
-import { Row, Col } from "react-bootstrap";
+import { Breadcrumb, Row, Col } from "react-bootstrap";
 import StationInfo from "./station-info";
 import CrimeTable from "~/crime-table";
 import { Link } from "react-router";
 
 export default function StationView(props: Route.ComponentProps["loaderData"]) {
   const { station, crimes, stats } = props;
-  const { name, former_name } = station.properties;
+  const { name, former_name, prov_code, prov_name } = station.properties;
   return (
     <main className="p-4">
-      <p>
-        <Link to="/">&larr; Back to map</Link>
-      </p>
+      <Breadcrumb>
+        <Breadcrumb.Item>
+          <Link to="/">Home</Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>
+          <Link to={`/province/${prov_code}`}>{prov_name}</Link>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item active>{name}</Breadcrumb.Item>
+      </Breadcrumb>
       <h2>
         Crime Stats for {name}
         {former_name && (
